4e987cb73111f776d8b5916f2000dfdad2e3c35f
Aaron Saray <me@aaronsaray.com>
2024-12-05 20:58:37 -0600

    Initial commit.

---
+++ README.md
@@ -0,0 +1,11 @@
+# 2020-2024 Ford Explorer Printable Manuals
+
+This software allows you to retrieve the posted manual data from [foexplorer.com](https://foexplorer.com) (at least at the time of publication) and create a printable PDF.
+
+## Instructions
+
+* Git checkout this project
+* Change into the directory
+* Run `composer install`
+* Run `php owners-manual.php` to generate an owners manual PDF in the local directory
+* Run `php service-manual.php` to generate a service manual PDF in the local directory
---
+++ app/BaseManual.php
@@ -0,0 +1,58 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App;
+
+use Mpdf\Mpdf;
+
+abstract class BaseManual
+{
+    abstract protected function urls(): array;
+
+    abstract protected function title(): string;
+
+    abstract protected function fileName(): string;
+
+    protected function getContents(string $file): string
+    {
+        return str_replace(
+            '{{ title }}',
+            $this->title(),
+            file_get_contents($file)
+        );
+    }
+
+    public function createPdf(): void
+    {
+        error_reporting(0);
+
+        $mpdf = new Mpdf();
+        $mpdf->setBasePath('https://www.foexplorer.com/');
+        $mpdf->WriteHTML($this->getContents(__DIR__ . '/../resources/owners-manual-cover.html'));
+        $mpdf->SetHTMLFooter('<div class="page">Page {PAGENO}</div>');
+        $mpdf->WriteHTML($this->getContents(__DIR__ . '/../resources/first-page.html'));
+
+        foreach ($this->urls() as $url) {
+            print "Processing $url";
+
+            $html = file_get_contents($url);
+            $start = stripos($html, '<div align="center">');
+            $end = stripos($html, '<div class="fodexp_rightblock">');
+
+            $first = substr($html, 0, $start);
+            $last = substr($html, $end);
+
+            $html = $first . $last;
+
+            $mpdf->WriteHTML($html);
+            $mpdf->WriteHTML('<div class="break-after-this"></div>');
+            print " Done.\n";
+        }
+
+        $file = $this->fileName();
+        $mpdf->Output($file);
+
+        print "File written to: {$file}\n\n";
+    }
+}
